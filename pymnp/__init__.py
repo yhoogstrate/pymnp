46c9fc921c7d0efd996585bb090854c6e57351aa
youri <y.hoogstrate@gmail.com>
2023-06-28 22:31:03 +0200

    packaging

---
+++ pymnp/__init__.py
@@ -0,0 +1,4 @@
+
+__version_info__ = ('0', '0', '1')
+__version__ = '.'.join(__version_info__) if (len(__version_info__) == 3) else '.'.join(__version_info__[0:3]) + "-" + __version_info__[3]
+
---
+++ setup.py
@@ -0,0 +1,31 @@
+#!/usr/env/bin python
+
+
+"""Install packages as defined in this file into the Python environment."""
+from setuptools import setup, find_packages
+
+# The version of this tool is based on the following steps:
+# https://packaging.python.org/guides/single-sourcing-package-version/
+VERSION = {}
+
+with open("./pymnp/__init__.py") as fp:
+    # pylint: disable=W0122
+    exec(fp.read(), VERSION)
+
+setup(
+    name="pymnp",
+    author="Dr. Youri Hoogstrate",
+    author_email="y.hoogstrate {at} erasmusmc dot nl",
+    url="https://github.com/yhoogstrate/pymnp",
+    description="pymnp: python api for the MolecularNeuropathology web portal",
+    version=VERSION.get("__version__", "0.0.0"),
+    packages=find_packages(where=".", exclude=["tests"]),
+    install_requires=[
+        "setuptools>=45.0",
+    ],
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Programming Language :: Python :: 3.0",
+        "Topic :: Utilities",
+    ],
+)
