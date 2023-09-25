b81ab6cd09da338a6cd1dad15fee686d1313b550
yhoogstrate <y.hoogstrate@gmail.com>
2023-09-25 13:28:24 +0200

    example

--- bin/api_example_download_all.py
+++ bin/api_example_download_all.py
@@ -12,6 +12,8 @@ def main():
     
     for sample in tqdm(app.update_samples()):
         for workflow in sample._workflows:
+            #if workflow._workflow_name_short == "brain_v12.8-S":
+
             if sample._workflows[workflow]['jobs'] is not None:
                 for job in sample._workflows[workflow]['jobs'].values():
                     if job.is_downloadable():
